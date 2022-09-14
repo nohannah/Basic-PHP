5c4e6edf34a3a016ac11994f16a16010e67d6761
No Hannah <67176677+nohannah@users.noreply.github.com>
2022-09-14 17:53:09 +0700

    14/09/2022

--- Basic_php.php
+++ Basic_php.php
@@ -12,3 +12,56 @@ echo "My first PHP script!"
 ?>
 </body>
 </html>
+<!DOCTYPE html>
+<html>
+<body>
+
+<?php
+echo "My first PHP script!"
+
+."<br>". 
+"my first php welcome"
+."<br>". 
+"This is my new website in this world"
+."<br>".
+"I am obsess with php!";
+echo "<hr>";
+//variable 
+$dog=12;
+$dad=57;
+echo"Dog's age:$dog<br>";
+
+echo"Dad's age:$dad<br>" ;
+
+?>
+<?php
+$sentence="I like apple ";
+$age=59;
+$score=80;
+echo $sentence;
+echo"Her father's age is $age<br>";
+echo"She got score:$score<br>";
+echo "<h1>Welcome to ISI</h1>";
+?>
+<p ">ISI STEEL started in 1997 as a small family-run roll former. It is now the industry leader in Cambodia's steel processing sector, having successfully transitioned into a midsize corporation.
+
+Our vision "to be a trusted leader in the processing, manufacturing, and distribution of quality and innovative steel building materials in Cambodia". We strive to achieve this through our core values of Hard Work, Integrity, and Innovation.</p>
+<table border=2 >
+<tr>
+	<td>Hello  </td>
+	<td> welcome</td>
+	<td> hope</td>
+	
+</tr>
+<tr>
+    <td>Alfreds Futterkiste</td>
+    <td>Maria Anders</td>
+    <td>Germany</td>
+  </tr>
+  <tr>
+    <td>Centro comercial Moctezuma</td>
+    <td>Francisco Chang</td>
+    <td>Mexico</td>
+</table>
+</body>
+</html>
