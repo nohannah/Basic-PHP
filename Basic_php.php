bcd4933af982866e724b66b414d307eeb1779d5c
No Hannah <67176677+nohannah@users.noreply.github.com>
2022-09-14 13:25:04 +0700

    14/9

---
+++ Basic_php.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<?php
+echo "My first PHP script!"
+
+."<br>".  Enter the sentence
+"my first php welcome"
+."<br>". 
+"This is my new website in this world";
+?>
+</body>
+</html>
