d24012c914fa75c083521ed5d8c0c0534c94dd59
No Hannah <67176677+nohannah@users.noreply.github.com>
2022-09-15 15:35:34 +0700

    15/09/2022
    1st develop

---
+++ Room booking1.php
@@ -0,0 +1,58 @@
+<!DOCTYPE html>
+<html>
+<head >
+<center>
+<h1>Booking meeting room <h1>
+</center>
+</head>
+<body style="background-color:olive">
+<hr>
+<label>Add Title</label>
+<input type="text"></input>
+<br>
+<br>
+<br>
+<button>Event</button> 
+
+<button>Task</button> 
+
+<button>Reminder</button>
+<br
+<p>From</p>
+<input type="date"></input>
+<input type="time"></input>
+<br>
+<br>
+<p>To</p>
+<input type="date"></input>
+<input type="time"></input>
+<br>
+<br>
+<label class="location" >Location</label>
+<style>
+.location{
+	font: verdanan , Arial; 
+	height:20px;
+	weight:50px;
+}
+</style>
+<input type="text"></input>
+<hr>
+<br>
+<label>Reminder<label>
+<select>
+		<option value="">5 miniute before</option>
+		<option value="">10 minuite before</option>
+		<option value="">15 minuite before</option>
+		
+		
+</select>
+<br>
+<br>
+<label>Add description</label>
+<input type="text"></input>
+
+
+
+</html>
+
